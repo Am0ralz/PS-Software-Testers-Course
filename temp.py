d44b26bb2b3434d8199e33ffa50a1c67b9a16d40
Angel Morales <Amorales211@users.noreply.github.com>
2018-11-15 22:20:30 -0500

    Delete temp.py

--- temp.py
+++
@@ -1,8 +0,0 @@
-
-option = "y"
-while option != "n":
-    temp1 = int(input("Enter temperature in Fahrenheit: "))
-    temp2 = (temp1-32)*(5/9)
-    print(temp1, "F")
-    print(temp2, "C")
-    option = input("Continue : yes: y or no: n ")
